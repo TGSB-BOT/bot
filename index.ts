ca0390f3d285978577dfb2bc499cd9c378667fe8
sportsr0bber <sportsbett0r@proton.me>
2023-08-20 00:43:01 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,12 +1,6 @@
 import { Main } from './bot/telegram/Main';
 import { IFunction } from './bot/telegram/IFunction';
 
-public MyFunc(obj : IStackOverFlow)
-{
-    // do stuff
-}
 
-var obj = {prop1: 'str', prop2: 3};
-MyFunc(obj); // ok
 
 export { Main };

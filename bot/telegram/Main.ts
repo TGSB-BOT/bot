e28869f0aba2e2e194029f51107cc25f0bf9fc5c
sportsr0bber <sportsbett0r@proton.me>
2023-08-19 19:55:52 +0200

    tgbot prepare

--- bot/telegram/Main.ts
+++ bot/telegram/Main.ts
@@ -1,5 +1,16 @@
 import { IFunction } from './IFunction';
 
+import config from "../../config.js";
+import TelegramBot from "node-telegram-bot-api";
+
+const bot_token = config.bot_token;
+const debug_channel = config.debug_channel;
+const debug_group = config.debug_group;
+const main_channel = config.main_channel;
+const main_debug = config.main_debug;
+
+const TgBot = new TelegramBot(bot_token, { polling: true });
+
 /**
  * 
  * @author ozzy_trades

1f02f9da702a2deec2fbaddbce9675e8f5906758
Ozzy <96839831+ozzytrades@users.noreply.github.com>
2023-08-19 20:34:06 +0200

    Update Main.ts

--- bot/telegram/Main.ts
+++ bot/telegram/Main.ts
@@ -2,6 +2,7 @@ import { IFunction } from './IFunction';
 import { Main as MainFromJS } from '../../Main';
 
 import config from "../../config.js";
+import TelegramBot from "node-telegram-bot-api";
 
 const bot_token = config.bot_token;
 const debug_channel = config.debug_channel;
@@ -9,10 +10,11 @@ const debug_group = config.debug_group;
 const main_channel = config.main_channel;
 const main_debug = config.main_debug;
 
+const TgBot = new TelegramBot(bot_token, { polling: true });
 
 /**
  * 
- * @author ozzy_trades, sportsr0bber
+ * @author ozzy_trades
  * / Main class for the bot integration 
  * 
  */
@@ -20,9 +22,7 @@ const main_debug = config.main_debug;
 class Main {
     functionInstance: IFunction;
 
-    useFunction() {
-        this.functionInstance.startTelegram();
-    }
+  
 }
 
 export { Main, MainFromJS };

526990586315a7f97a5974b36961083c8028e998
sportsr0bber <142682110+sportsr0bber@users.noreply.github.com>
2023-08-19 18:51:08 +0200

    config added

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-// test
+const config = require("./config");
+const TelegramBot = require("node-telegram-bot-api");
+
+const bot_id = config.bot_token;
+const debug_channel = config.debug_channel;
+const debug_group = config.debug_group;
+const main_channel = config.main_channel;
+const main_debug = config.main_debug;
+
+const TgBot = new TelegramBot(bot_id, { polling: true });
